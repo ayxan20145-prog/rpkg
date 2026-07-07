80c926d20fa564b1a563be5e0e279651b1247daf
ayxan <ayxan.20145@gmail.com>
2026-07-07 22:29:51 +0400

    you can now install packages

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-use std::env;
+use std::{env, fs};
 
-fn main() {
-    match env::args().nth(1).as_deref() {
-        Some("install") => println!("No packages yet :("),
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    match args.get(1).map(String::as_str) {
+        Some("install") => match args.get(2).map(String::as_str) {
+            Some(pkg) => {
+                let url = format!(
+                    "https://raw.githubusercontent.com/ayxan20145-prog/tarman-pkgs/main/packages/{}.tar.gz",
+                    pkg
+                );
+                let bytes = reqwest::blocking::get(&url)?.bytes()?;
+                let pkg_name = format!("{}.tar.gz", pkg);
+
+                fs::write(pkg_name, &bytes)?;
+            }
+            None => println!("Usage: tarman install <package>"),
+        },
         Some(cmd) => println!("Unknown command: {}", cmd),
-        None => println!("Usage: \ntarman install <package>"),
+        None => println!("Usage: tarman install <package>"),
     }
+
+    Ok(())
 }
