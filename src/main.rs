a832b9000ae1711660e2caa0d5de2a3358dc5ff0
ayxan <ayxan.20145@gmail.com>
2026-07-08 15:00:08 +0400

    added updating and better installing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     env, fs,
     io::{self, Write},
+    path::PathBuf,
     process::Command,
 };
 
@@ -10,45 +11,95 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     match args.get(1).map(String::as_str) {
         Some("install") => match args.get(2).map(String::as_str) {
             Some(pkg) => {
-                let url = format!(
-                    "https://raw.githubusercontent.com/ayxan20145-prog/tarman-pkgs/main/packages/{}.tar.gz",
-                    pkg
+                let dir = PathBuf::from(std::env::var("HOME")?)
+                    .join(".local")
+                    .join("share")
+                    .join("rpkg");
+
+                let index = fs::read_to_string(dir.join("index"))?;
+
+                let mut found = false;
+
+                let mut name = String::new();
+                let mut description = String::new();
+                let mut version = String::new();
+                let mut url = String::new();
+
+                for line in index.lines() {
+                    let line = line.trim();
+
+                    if !found {
+                        if line == pkg {
+                            found = true;
+                            name = line.to_string();
+                        }
+                        continue;
+                    }
+
+                    if line == "end" {
+                        break;
+                    }
+
+                    if let Some(value) = line.strip_prefix("description ") {
+                        description = value.to_string();
+                    } else if let Some(value) = line.strip_prefix("version ") {
+                        version = value.to_string();
+                    } else if let Some(value) = line.strip_prefix("url ") {
+                        url = value.to_string();
+                    }
+                }
+
+                if !found {
+                    println!("Package '{}' not found.", pkg);
+                    return Ok(());
+                }
+
+                println!(
+                    "Package found\n Name: {}\n Description: {}\n Version: {}",
+                    name, description, version
                 );
-                let bytes = reqwest::blocking::get(&url)?.bytes()?;
-                let pkg_name = format!("{}.tar.gz", pkg);
 
-                fs::write(&pkg_name, &bytes)?;
-
-                print!("Extract tar.gz file? (y/n): ");
+                print!("\nInstall? (y/n): ");
                 io::stdout().flush()?;
                 let mut choice = String::new();
                 io::stdin().read_line(&mut choice)?;
+                if choice.trim() == "y" {
+                    println!("Installing...");
+                    let bytes = reqwest::blocking::get(&url)?.bytes()?;
+                    let pkg_name = format!("{name}.tar.gz");
 
-                if choice.trim().to_lowercase() == "y" {
+                    fs::write(&pkg_name, &bytes)?;
+
+                    println!("Extracting...");
                     let extract = Command::new("tar").arg("-xf").arg(&pkg_name).status()?;
 
                     if !extract.success() {
-                        eprintln!("Extract failed");
+                        return Err("Extract failed".into());
                     }
-                }
 
-                print!("Remove tar.gz file? (y/n): ");
-                io::stdout().flush()?;
-                let mut choice = String::new();
-                io::stdin().read_line(&mut choice)?;
+                    println!("Removing tar.gz file...");
+                    fs::remove_file(&pkg_name)?;
 
-                if choice.trim().to_lowercase() == "y" {
-                    let remove = Command::new("rm").arg(&pkg_name).status()?;
-
-                    if !remove.success() {
-                        eprintln!("Remove failed");
-                    }
+                    println!("\nDone!");
                 }
             }
-            None => println!("Usage: tarman install <package>"),
+            None => println!("Usage: rpkg install <package>"),
         },
+        Some("update") => {
+            let dir = PathBuf::from(std::env::var("HOME")?)
+                .join(".local")
+                .join("share")
+                .join("rpkg");
+
+            fs::create_dir_all(&dir)?;
+
+            let url = "https://raw.githubusercontent.com/ayxan20145-prog/rpkgs/main/index";
+            let bytes = reqwest::blocking::get(url)?.bytes()?;
+
+            fs::write(dir.join("index"), &bytes)?;
+        }
         Some(cmd) => println!("Unknown command: {}", cmd),
-        None => println!("Usage: tarman install <package>"),
+        None => println!("Usage: \nrpkg install <package>\nrpkg update"),
     }
 
     Ok(())
